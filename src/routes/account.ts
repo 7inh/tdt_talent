9557755a5935edb00d3f493341d01626fa2d18a4
7inh <tranquoclinh247@gmail.com>
2022-05-15 02:52:23 +0700

    update import

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,5 +1,5 @@
 import MiddleWare from "src/utils/MiddleWare";
-import AccountController from "src/controllers/AccountController";
+import AccountController from "src/controllers/account.controller";
 
 const express = require("express");
 const account = express.Router();
