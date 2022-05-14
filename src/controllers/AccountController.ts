23c138c3655757dd3d94731cc5f67f3dd6e75e91
7inh <tranquoclinh247@gmail.com>
2022-05-14 23:28:39 +0700

    fix return

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -3,7 +3,7 @@ import { NextType, RequestType, ResponseType } from "src/utils/types";
 class AccountController {
     async login(req: RequestType, res: ResponseType, next: NextType) {
         try {
-            res.status(200).json(req.user);
+            return res.status(200).json(req.user);
         } catch (e) {
             const err = new Error("Unauthorized");
             return next(err);
