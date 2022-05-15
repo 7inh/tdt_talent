05e13a8cad74378d856179e6c644ec4836c5d3c8
7inh <tranquoclinh247@gmail.com>
2022-05-15 11:54:21 +0700

    update definitions

--- src/utils/definitions/index.ts
+++ src/utils/definitions/index.ts
@@ -1,19 +1,17 @@
-export const ERROR_DETAIL = {
-    "INVALID_EMAIL": {
-        message: "Invalid email address",
-        status: 403
-    },
-    "BAD_REQUEST": {
-        message: "Bad request",
-        status: 400
-    }
-
-}
-
 export const ERROR_MESSAGE = {
     INVALID_EMAIL: "INVALID_EMAIL",
-    BAD_REQUEST: "BAD_REQUEST"
-    
-}
+    BAD_REQUEST: "BAD_REQUEST",
+};
 
-export const VALID_EMAIL = '@gmail.com'
+export const ERROR_DETAIL = {
+    [ERROR_MESSAGE.BAD_REQUEST]: {
+        message: "Bad request",
+        status: 400,
+    },
+    [ERROR_MESSAGE.INVALID_EMAIL]: {
+        message: "Invalid email address",
+        status: 403,
+    },
+};
+
+export const VALID_EMAIL = "@gmail.com";
