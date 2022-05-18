d62d7b06a21699fa3667fcd277890c6f108c4931
7inh <tranquoclinh247@gmail.com>
2022-05-18 22:16:50 +0700

    update account query

--- src/services/account-service/account.query.ts
+++ src/services/account-service/account.query.ts
@@ -2,7 +2,7 @@ import database from "src/database/database";
 
 export async function getAccountByEmail(email: string) {
     return await database("account")
-        .select(["email", "role"])
+        .select(["email", "role", "id"])
         .where({
             email,
             deleted_at: null,
