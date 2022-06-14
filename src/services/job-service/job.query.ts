238d34eb96785fad51ad061312a7936297cf278e
7inh <tranquoclinh247@gmail.com>
2022-06-15 00:31:32 +0700

    admin manage job

--- src/services/job-service/job.query.ts
+++ src/services/job-service/job.query.ts
@@ -1,7 +1,19 @@
 import database from "src/database/database";
 
 export async function getAllJob() {
-    return await database("job").select("*").orderBy("created_at", "desc");
+    return await database("job")
+        .select({
+            title: "job.title",
+            company_name: "profile.full_name",
+            company_avatar: "profile.avatar_url",
+            created_at: "job.created_at",
+            location: "job.location",
+            position: "position.title",
+            employment_type: "job.employment_type",
+        })
+        .join("profile", "profile.account_id", "job.account_id")
+        .join("position", "position.id", "job.position_id")
+        .orderBy("created_at", "desc");
 }
 
 export async function getByCompany(id: number) {
@@ -15,7 +27,7 @@ export async function getByCompany(id: number) {
         .leftJoin("application", "application.job_id", "job.id")
         .count({ total: "application.id" })
         .where({ "job.account_id": id })
-        .groupBy("job.state","job.created_at", "job.title", "job.id")
+        .groupBy("job.state", "job.created_at", "job.title", "job.id")
         .orderBy("job.created_at", "desc");
 }
 
